ef76ce67dbb9574c76b4f5b5d72131a5d5f1f91b
stefanBid <biddau.stefano99@gmail.com>
2026-07-15 18:09:49 +0200

    feat: add caching support and execution time logging for service methods

--- src/main/java/com/sb/sfrigola_core/domains/languages/repository/ILanguageRepository.java
+++ src/main/java/com/sb/sfrigola_core/domains/languages/repository/ILanguageRepository.java
@@ -1,15 +1,12 @@
 package com.sb.sfrigola_core.domains.languages.repository;
 
 import com.sb.sfrigola_core.domains.languages.entity.Language;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ILanguageRepository extends JpaRepository<Language, Long> {
 
-    Page<Language> findAllByIsActiveTrue(Pageable pageable);
     List<Language> findAllByIsActiveTrue();
     boolean existsByCode(String code);
     boolean existsByCodeAndIsActiveTrue(String code);
